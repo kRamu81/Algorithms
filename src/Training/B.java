a9d6b6899fda80ce184f7c230c3f23b68be3400a
kRamu81 <kanamramu18@gmail.com>
2026-02-15 07:00:05 +0530

    Day 21 Exception Handling

---
+++ src/Training/B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class B {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        long area = a*b;
+        System.out.println(area);
+    }
+}
